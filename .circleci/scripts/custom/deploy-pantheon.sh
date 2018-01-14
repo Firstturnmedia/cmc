f56063e883aaeca647ae48e40dfba1b62d83cfe6
ftm-bill <bill@firstturnmedia.com>
2018-01-14 14:44:38 -0500

    added updated circleci config + deploy-pantheon.sh

---
+++ .circleci/scripts/custom/deploy-pantheon.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Make sure feature branch names work with Pantheon's 11 char max.
+
+# Multidev
+if [ "$CIRCLE_BRANCH" != "master" ]; then
+  # Log in w/ terminus
+  terminus -n auth:login --machine-token="$TERMINUS_TOKEN"
+
+  # Check if multidev aleeady exists. If not, create, else, update existing
+  if ! terminus multidev:list $TERMINUS_SITE --field id | grep $CIRCLE_BRANCH; then
+    echo "Creating multidev $CIRCLE_BRANCH-$TERMINUS_SITE.dev"
+    terminus env:wake "$TERMINUS_SITE.dev"
+    terminus multidev:create $TERMINUS_SITE.dev $CIRCLE_BRANCH
+    else
+      echo "Existing multidev $CIRCLE_BRANCH-$TERMINUS_SITE will be updated."
+  fi
+
+  # Remove any existing build
+  rm -rf /tmp/pantheon
+
+  # Move to /tmp
+  cd /tmp
+
+  # Git clone from Pantheon
+  git clone -b $CIRCLE_BRANCH ssh://codeserver.dev.$PANTHEON_SITE@codeserver.dev.$PANTHEON_SITE.drush.in:2222/~/repository.git pantheon
+
+  # Delete everything except the .git dir
+  rm -rf /tmp/pantheon/*
+
+  # Copy "full repo" code over
+  cp -rf /root/drops_8_composer/* /tmp/pantheon/
+
+  # Move to pantheon dir
+  cd /tmp/pantheon
+
+  # Setup git user.email and user.name
+  git config user.email "${GIT_EMAIL}" && git config user.name "${CIRCLE_USERNAME}"
+
+  # Git add and commit
+  git add -A
+  git commit -m "${GIT_COMMIT_DESC}"
+
+  # Push code to multidev
+  git push -f origin $CIRCLE_BRANCH
+
+  # Run update.php
+  # Run config-import -y
+  # Clear drupal cache
+fi
+
+# Master branch
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+  echo "$TERMINUS_SITE.dev will be updated."
+  # Log in w/ terminus
+  terminus -n auth:login --machine-token="$TERMINUS_TOKEN"
+
+  # Remove any existing build
+  rm -rf /tmp/pantheon
+
+  # Move to /tmp
+  cd /tmp
+
+  # Git clone from Pantheon
+  git clone ssh://codeserver.dev.$PANTHEON_SITE@codeserver.dev.$PANTHEON_SITE.drush.in:2222/~/repository.git pantheon
+
+  # Delete everything except the .git dir
+  rm -rf /tmp/pantheon/*
+
+  # Copy "full repo" code over
+  cp -rf /root/drops_8_composer/* /tmp/pantheon/
+
+  # Move to pantheon dir
+  cd /tmp/pantheon
+
+  # Setup git user.email and user.name
+  git config user.email "${GIT_EMAIL}" && git config user.name "${CIRCLE_USERNAME}"
+
+  # Git add and commit
+  git add -A
+  git commit -m "$GIT_COMMIT_DESC"
+
+  # Push code to dev
+  git push -f origin master
+fi
