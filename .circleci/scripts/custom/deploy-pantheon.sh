a62f95e76730d257051e9252a88941eaeabe5194
ftm-bill <bill@firstturnmedia.com>
2018-02-12 17:37:33 -0500

    will it work?

--- .circleci/scripts/custom/deploy-pantheon.sh
+++ .circleci/scripts/custom/deploy-pantheon.sh
@@ -80,9 +80,12 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
   # Setup git user.email and user.name
   git config user.email "${GIT_EMAIL}" && git config user.name "${CIRCLE_USERNAME}"
 
+  # testing
+  echo "Git commit msg: $GIT_COMMIT_DESC"
+
   # Git add and commit
   git add -A
-  git commit -m "Circle CI Build: ${CIRCLE_BUILD_URL}"
+  git commit -m "Circle CI Build: ${CIRCLE_BUILD_URL} Git commit msg: ${GIT_COMMIT_DESC}"
 
   # Push code to dev
   git push -f origin master
