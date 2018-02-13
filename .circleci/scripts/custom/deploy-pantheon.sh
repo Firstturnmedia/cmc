5aad8ec54a9218bc5b47762f88d8c0846cb3ac63
ftm-bill <bill@firstturnmedia.com>
2018-02-12 20:02:21 -0500

    test commit msg

--- .circleci/scripts/custom/deploy-pantheon.sh
+++ .circleci/scripts/custom/deploy-pantheon.sh
@@ -95,8 +95,9 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
 
   # Git add and commit
   git add -A
-  git commit -m "Circle CI Build: ${CIRCLE_BUILD_URL} Git commit msg: ${GIT_COMMIT_DESC}"
-
+  #git commit -m "Circle CI Build: ${CIRCLE_BUILD_URL} Git commit msg: ${GIT_COMMIT_DESC}"
+  git commit -m "Circle CI Build: $CIRCLE_BUILD_URL"
+  
   # Push code to dev
   git push -f origin master
 
